9f9ab04b7f28bc57d25d6c2c3ab6dab638c05163
Brdekrom <brdekrom@gmail.com>
2023-10-27 19:07:21 +0200

    Add Infra DI

---
+++ CodeSense.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using CodeSense.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSense.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInMemoryDatabase(this IServiceCollection services)
+    {
+        services.AddDbContext<CodeSenseDbContext>(options => options.UseInMemoryDatabase("MyInMemoryDb"));
+        return services;
+    }
+}
