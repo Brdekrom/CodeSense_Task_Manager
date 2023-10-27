1c162289215be16a22dac1578570b0931ab1710b
Brdekrom <brdekrom@gmail.com>
2023-10-27 19:32:02 +0200

    Add DI

--- CodeSense.Infrastructure/DependencyInjection.cs
+++ CodeSense.Infrastructure/DependencyInjection.cs
@@ -1,11 +1,7 @@
 ﻿using CodeSense.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace CodeSense.Infrastructure;
 
