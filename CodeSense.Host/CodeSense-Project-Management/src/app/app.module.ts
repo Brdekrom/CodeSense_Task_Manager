ac3d4e9a9347c2573ce6424b82df14f70f34672c
Brdekrom <brdekrom@gmail.com>
2023-12-29 20:57:52 +0100

    add latest changes

--- CodeSense.Host/CodeSense-Project-Management/src/app/app.module.ts
+++ CodeSense.Host/CodeSense-Project-Management/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HomeComponent } from './components/home/home.component';
 import { ProjectInputComponent } from './components/project-input/project-input.component';
 import { LoginComponent } from './components/login/login.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { AuthModule } from '@auth0/auth0-angular';
 
 @NgModule({
   declarations: [
@@ -15,12 +16,20 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     HomeComponent,
     ProjectInputComponent,
     LoginComponent,
-    NavbarComponent
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+
+    AuthModule.forRoot({
+      domain: 'dev-xlkommxvsey67l3c.us.auth0.com',
+      clientId: '2ZZ0yuI2hpDEA7UuVIiVP61V2I7ry70L',
+      authorizationParams: {
+        redirect_uri: window.location.origin
+      }
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- CodeSense.Host/CodeSense-Project-Management/src/app/components/navbar/navbar.component.ts
+++ CodeSense.Host/CodeSense-Project-Management/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'navbar',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.sass']
 })
 export class NavbarComponent {
+
+  constructor(public _auth:AuthService) { }
+
+  ngOnInit(): void {
+  }
 
 }
