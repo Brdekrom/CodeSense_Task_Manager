47a9b5eccd47c91f386e2041b464c4c68353ccd9
Bryan <dekrobr@cronos.be>
2024-01-04 14:42:07 +0100

    add successfull registration behavior

--- CodeSense.Host/CodeSense-Project-Management/src/app/components/user/register-user/register-user.component.ts
+++ CodeSense.Host/CodeSense-Project-Management/src/app/components/user/register-user/register-user.component.ts
@@ -2,6 +2,7 @@ import { Component, } from '@angular/core';
 import { UserService } from 'src/app/services/users/user.service';
 import { FormControl, FormGroup } from '@angular/forms';
 import { User } from 'src/app/interfaces/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-user',
@@ -10,7 +11,10 @@ import { User } from 'src/app/interfaces/user';
 })
 export class RegisterUserComponent {
 
-  constructor(private userService: UserService) { }
+  successMessage: string | null = null;
+  errorMessage: string | null = null;
+
+  constructor(private userService: UserService, private router: Router) { }
 
   registerForm = new FormGroup({
     firstName: new FormControl(''),
@@ -33,9 +37,21 @@ export class RegisterUserComponent {
   }
 
   onSubmit() {
+    this.successMessage = null;
+    this.errorMessage = null;
+
     const user = this.mapUser();
-    this.userService.createUser(user).subscribe((result) => {
-      console.warn("result", result)
-    })
+    this.userService.createUser(user).subscribe({
+      next: (result) => {
+        console.warn("result", result);
+        this.successMessage = "User successfully registered.";
+        this.registerForm.reset(); // Reset form on successful submission
+        this.router.navigate(['/login']); // Navigate to home page on successful submission
+      },
+      error: (error) => {
+        console.error("Error", error);
+        this.errorMessage = "There was an error registering the user.";
+      }
+    });
   }
 }
